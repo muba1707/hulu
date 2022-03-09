7d976410e064dd865ddc71d3a5d8c2d6cdc2a258
muba@1707 <batcha.1707@gmail.com>
2022-03-09 14:40:11 +0530

    small change

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
-import { forwardRef } from "react/cjs/react.development";
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = ({ result }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
@@ -33,6 +32,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       </div>
     </div>
   );
-});
+};
 
 export default Thumbnail;
